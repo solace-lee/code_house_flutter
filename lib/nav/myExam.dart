9a0ab74a4cf22e2fcc3f4006920f67689448515a
李祖明 <solace.lee@outlook.com>
2020-08-21 22:34:07 +0800

    细节变动

--- lib/nav/myExam.dart
+++ lib/nav/myExam.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 //     final argments;
 //     MyExam({ this.argments });
 
-
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
@@ -22,7 +21,7 @@ import 'package:flutter/material.dart';
 
 class MyExam extends StatefulWidget {
   final Map arguments;
-  MyExam({Key key, this.arguments}) : super(key : key);
+  MyExam({Key key, this.arguments}) : super(key: key);
   @override
   _MyExamState createState() => _MyExamState(arguments: this.arguments);
 }
@@ -33,16 +32,39 @@ class _MyExamState extends State<MyExam> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(arguments != null ? arguments['title'] : '没有标题')
-      ),
-      body: Container(
-       child: RaisedButton(
-        child: Text('返回'),
-        onPressed: () {
-          Navigator.of(context).pop();
-       }),
-      )
-    );
+        appBar: AppBar(
+            title: Text(arguments != null ? arguments['title'] : '没有标题')),
+        drawer: Drawer(
+            child: Column(
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          "https://www.itying.com/images/flutter/2.png"),
+                      fit: BoxFit.cover)),
+              child: ListView(
+                children: <Widget>[Text('我是一个头部')],
+              ),
+            ),
+            ListTile(
+              title: Text("个人中心"),
+              leading: CircleAvatar(child: Icon(Icons.people)),
+            ),
+            Divider(),
+            ListTile(
+              title: Text("系统设置"),
+              leading: CircleAvatar(child: Icon(Icons.settings)),
+            )
+          ],
+        )),
+        body: Container(
+          child: RaisedButton(
+              child: Text('返回'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              }),
+        ));
   }
 }
