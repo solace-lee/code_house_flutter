15353441e26d104d3218ccb43f1b3d65a53e53db
李祖明 <solace.lee@outlook.com>
2020-08-26 22:37:37 +0800

    全局变量

--- lib/routes/myExam.dart
+++ lib/routes/myExam.dart
@@ -14,6 +14,11 @@ class _MyExamState extends State<MyExam> {
   bool _switchSelected = true; // 维护单选开关状态
   bool _checkboxSelected = true; // 维护复选框状态
 
+
+  TextEditingController _unameController = new TextEditingController();
+  TextEditingController _pwdController = new TextEditingController();
+  GlobalKey _formKey = new GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,12 +59,80 @@ class _MyExamState extends State<MyExam> {
             )
           ],
         )),
-        body: Container(
-          child: RaisedButton(
-              child: Text('返回'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              }),
-        ));
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal:24.0),
+          child: Form(
+            key: _formKey, // 设置globalKey，用于后面获取FormState
+            autovalidate: true, // 开启自动校验
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  autofocus: true,
+                  controller: _unameController,
+                  decoration: InputDecoration(
+                    labelText: "用户名",
+                    hintText: "用户名或邮箱",
+                    icon: Icon(Icons.person)
+                  ),
+                  validator: (v) {
+                    return v
+                            .trim()
+                            .length > 0 ? null : "用户名不能为空";
+                  }
+                ),
+                TextFormField(
+                  controller: _pwdController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "密码",
+                    hintText: "您的登录密码",
+                    icon: Icon(Icons.lock)
+                  ),
+                  validator: (v) {
+                    return v
+                            .trim()
+                            .length > 5 ? null : "密码不能少于6位";
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 28.0, bottom: 28.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: RaisedButton(
+                          padding: EdgeInsets.all(15.0),
+                          child: Text("登录"),
+                          color: Theme.of(context).primaryColor,
+                          textColor: Colors.white,
+                          onPressed: () {
+                            print(_formKey.currentState.toString());
+                            if ((_formKey.currentState as FormState).validate()) {
+                              print('可以提交数据了');
+                            }
+                          }
+                        )
+                      )
+                    ],
+                  ),
+                ),
+                Builder(builder: (context) {
+                  return RaisedButton(
+                      padding: EdgeInsets.all(15.0),
+                      child: Text("登录"),
+                      color: Theme.of(context).primaryColor,
+                      textColor: Colors.white,
+                      onPressed: () {
+                        print(_formKey.currentState.toString());
+                        if (Form.of(context).validate()) {
+                          print('可以提交数据了');
+                        }
+                      }
+                    );
+                }),
+              ],
+            ),
+          ),
+        ),
+    );
   }
 }
